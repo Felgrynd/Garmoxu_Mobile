14bfd00b2c3fbf401e05997c681cd51a672c081c
Felgrynd <axj201516@gmail.com>
2022-04-30 18:13:54 +0200

    v0.0.3
    -Nada

--- Garmoxu_Mobile/app/src/main/java/pruebas/com/garmoxu_mobile/MainActivity.java
+++ Garmoxu_Mobile/app/src/main/java/pruebas/com/garmoxu_mobile/MainActivity.java
@@ -6,13 +6,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+<<<<<<< HEAD
     String yo = "Hola";
 
+=======
+>>>>>>> parent of 97662d3 (v.0.0.1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 }
