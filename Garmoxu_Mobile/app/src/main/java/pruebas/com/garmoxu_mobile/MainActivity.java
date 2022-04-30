97662d3beca3fae5dd0ef4553b802bc4ff003859
juliangasal46 <juliangasal46@gmail.com>
2022-04-30 17:45:12 +0200

    v.0.0.1
    - Pruebas de commit

--- Garmoxu_Mobile/app/src/main/java/pruebas/com/garmoxu_mobile/MainActivity.java
+++ Garmoxu_Mobile/app/src/main/java/pruebas/com/garmoxu_mobile/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String ñoclok = "Supapinga";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
