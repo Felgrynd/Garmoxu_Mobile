26b39f9961f14e9fa7344418e03cb2f0767ce8d0
Felgrynd <axj201516@gmail.com>
2022-04-30 18:17:42 +0200

    Revert "v.0.0.1"
    This reverts commit 97662d3beca3fae5dd0ef4553b802bc4ff003859.

--- Garmoxu_Mobile/app/src/main/java/pruebas/com/garmoxu_mobile/MainActivity.java
+++ Garmoxu_Mobile/app/src/main/java/pruebas/com/garmoxu_mobile/MainActivity.java
@@ -6,13 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    String ñoclok = "Supapinga";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 }
