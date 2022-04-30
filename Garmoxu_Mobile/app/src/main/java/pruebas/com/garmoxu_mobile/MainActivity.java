4bf2ea8794c4a33bf6acfe492d4f97cd1fb6f0a6
juliangasal46 <juliangasal46@gmail.com>
2022-04-30 18:37:13 +0200

    v0.0.4
    Añadido num

--- Garmoxu_Mobile/app/src/main/java/pruebas/com/garmoxu_mobile/MainActivity.java
+++ Garmoxu_Mobile/app/src/main/java/pruebas/com/garmoxu_mobile/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     String version = "0.0.3-rama";
+    int pruebaReal = 69;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
