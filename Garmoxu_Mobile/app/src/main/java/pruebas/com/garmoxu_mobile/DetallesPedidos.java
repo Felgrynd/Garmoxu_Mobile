f7a2b7a7fb0704a1707778965f8d72c8acaea26c
juliangasal46 <juliangasal46@gmail.com>
2022-05-07 20:19:40 +0200

    v0.1.0

---
+++ Garmoxu_Mobile/app/src/main/java/pruebas/com/garmoxu_mobile/DetallesPedidos.java
@@ -0,0 +1,14 @@
+package pruebas.com.garmoxu_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetallesPedidos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_pedidos);
+    }
+}
